18c4fcac79dd4837706358b1f2ef7069f7c07b8c
Ristarg <a.filippov@protonmail.com>
2018-05-29 23:47:09 +0700

    Cut down on grammar, basic lexer done

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,77 @@
 #[derive(Debug, PartialEq)]
 enum Token {
-    Int(i64),
+    Unknown,
+    Int(u8),
+    PlusSign,
+    OpenBracket,
+    CloseBracket,
+    Space
+}
+
+struct SourceReader<'a> {
+    source: &'a [u8],
+    idx: usize,
+}
+
+impl<'a> SourceReader<'a> {
+    fn new(source: &str) -> SourceReader {
+        SourceReader {
+            source: source.as_bytes(),
+            idx: 0
+        }
+    }
+
+    fn next_token(&mut self) -> Token {
+        let cur_char = self.source[self.idx];
+        let token = match cur_char {
+            b'0'...b'9' => Token::Int(cur_char - b'0'),
+            b'+' => Token::PlusSign,
+            b'(' => Token::OpenBracket,
+            b')' => Token::CloseBracket,
+            b' ' => Token::Space,
+            _ => Token::Unknown,
+        };
+
+        self.idx += 1;
+        token
+    }
 }
 
 fn main() {
-    let source = load_source();
-    let tokens = tokenize_source(&source);
-    println!("{:?}", tokens);
-}
+    let mut rdr = SourceReader::new("(+ 1 2)");
 
-fn load_source() -> String {
-    "1".to_owned()
-}
-
-fn tokenize_source(source: &str) -> Vec<Token> {
-    let mut tokens = vec![];
-    let mut reader = source.chars().peekable();
-
-    while let Some(&next_char) = reader.peek() {
-        match next_char {
-            '0'...'9' | '-' => {
-                let digits: String = (&mut reader)
-                    .take_while(|c| c.is_ascii_digit() || *c == '-') //FIXME: only take the first minus
-                    .collect();
-                let token = Token::Int(digits.parse().unwrap());
-                tokens.push(token);
-            }
-            _ => {
-                println!("Unrecognized token: \"{}\", skipping...", next_char);
-                reader.next();
-            }
-        };
+    while rdr.idx < rdr.source.len() {
+        let token = rdr.next_token();
+        println!("Token: {:?}", token);
     }
-
-    tokens
 }
 
 #[test]
-fn test_lexer_single_digit_ints() {
-    assert_eq!(tokenize_source("0"), vec![Token::Int(0)]);
-    assert_eq!(tokenize_source("1"), vec![Token::Int(1)]);
-    assert_eq!(tokenize_source("2"), vec![Token::Int(2)]);
-    assert_eq!(tokenize_source("3"), vec![Token::Int(3)]);
-    assert_eq!(tokenize_source("4"), vec![Token::Int(4)]);
-    assert_eq!(tokenize_source("5"), vec![Token::Int(5)]);
-    assert_eq!(tokenize_source("6"), vec![Token::Int(6)]);
-    assert_eq!(tokenize_source("7"), vec![Token::Int(7)]);
-    assert_eq!(tokenize_source("8"), vec![Token::Int(8)]);
-    assert_eq!(tokenize_source("9"), vec![Token::Int(9)]);
+fn test_lexer_integers() {
+    let mut rdr = SourceReader::new("0123456789");
+
+    assert_eq!(rdr.next_token(), Token::Int(0));
+    assert_eq!(rdr.next_token(), Token::Int(1));
+    assert_eq!(rdr.next_token(), Token::Int(2));
+    assert_eq!(rdr.next_token(), Token::Int(3));
+    assert_eq!(rdr.next_token(), Token::Int(4));
+    assert_eq!(rdr.next_token(), Token::Int(5));
+    assert_eq!(rdr.next_token(), Token::Int(6));
+    assert_eq!(rdr.next_token(), Token::Int(7));
+    assert_eq!(rdr.next_token(), Token::Int(8));
+    assert_eq!(rdr.next_token(), Token::Int(9));
 }
 
 #[test]
-fn test_lexer_multiple_digit_ints() {
-    assert_eq!(tokenize_source("10"), vec![Token::Int(10)]);
-    assert_eq!(tokenize_source("1456"), vec![Token::Int(1456)]);
-    assert_eq!(tokenize_source("234234"), vec![Token::Int(234234)]);
+fn test_lexer_singletons() {
+    assert_eq!(SourceReader::new("+").next_token(), Token::PlusSign);
+    assert_eq!(SourceReader::new("(").next_token(), Token::OpenBracket);
+    assert_eq!(SourceReader::new(")").next_token(), Token::CloseBracket);
+    assert_eq!(SourceReader::new(" ").next_token(), Token::Space);
 }
 
 #[test]
-fn test_lexer_negative_ints() {
-    assert_eq!(tokenize_source("-10"), vec![Token::Int(-10)]);
-    assert_eq!(tokenize_source("-1456"), vec![Token::Int(-1456)]);
-    assert_eq!(tokenize_source("-234234"), vec![Token::Int(-234234)]);
+fn test_lexer_unknown_characters() {
+    assert_eq!(SourceReader::new("a").next_token(), Token::Unknown);
+    assert_eq!(SourceReader::new("$").next_token(), Token::Unknown);
 }
