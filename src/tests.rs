2812f7e5cfa9122b9cb34d05a3140137dfbdc09e
Ristarg <a.filippov@protonmail.com>
2018-05-30 17:38:36 +0700

    Fixed tests

--- src/tests.rs
+++ src/tests.rs
@@ -72,82 +72,40 @@ fn test_parser_expressions() {
     assert_eq!(expr, Expr::Number(1));
 
     let expr = Tokenizer::new("(+ 1 2)").parse_expression();
-    assert_eq!(expr, Expr::BinExpr {
+    assert_eq!(expr, Expr::BinExpr(Box::new(BinExpr {
         op: Operator::Add,
         left: Expr::Number(1),
         right: Expr::Number(2)
-    });
+    })));
 
-    // let expr = Tokenizer::new("(* (+ 123 565) (* (+ 12 3) 134))").parse_expression();
-    // assert_eq!(expr, Expr::BinExpr {
-    //     op: Operator::Multiply,
-    //     left: Expr::BinExpr {
-    //         op: Operator::Add,
-    //         left: Expr::Number(123),
-    //         right: Expr::Number(565)
-    //     },
-    //     right: Expr::BinExpr {
-    //         op: Operator::Multiply,
-    //         left: BinExpr {
-    //             op: Operator::Add,
-    //             left: Expr::Number(12),
-    //             right: Expr::Number(3)
-    //         },
-    //         right: Expr::Number(134)
-    //     }
-    // });
+    let expr = Tokenizer::new("(* (+ 123 565) (* (+ 12 3) 134))").parse_expression();
+    assert_eq!(expr, Expr::BinExpr(Box::new(BinExpr {
+        op: Operator::Multiply,
+        left: Expr::BinExpr(Box::new(BinExpr {
+            op: Operator::Add,
+            left: Expr::Number(123),
+            right: Expr::Number(565)
+        })),
+        right: Expr::BinExpr(Box::new(BinExpr {
+            op: Operator::Multiply,
+            left: Expr::BinExpr(Box::new(BinExpr {
+                op: Operator::Add,
+                left: Expr::Number(12),
+                right: Expr::Number(3)
+            })),
+            right: Expr::Number(134)
+        }))
+    })));
 }
 
-// #[test]
-// fn test_eval() {
-//     //FIXME: recursion broke everything, fix this shit
-//     assert_eq!(
-//         eval(&BinExpr {
-//             op: Operator::Add,
-//             left: 9,
-//             right: 0
-//         }),
-//         9
-//     );
-//     assert_eq!(
-//         eval(&BinExpr {
-//             op: Operator::Add,
-//             left: 4,
-//             right: 1
-//         }),
-//         5
-//     );
-//     assert_eq!(
-//         eval(&BinExpr {
-//             op: Operator::Add,
-//             left: 3,
-//             right: 5
-//         }),
-//         8
-//     );
+#[test]
+fn test_eval() {
+    assert_eq!(eval(&Tokenizer::new("(+ 9 0)").parse_expression()), 9);
+    assert_eq!(eval(&Tokenizer::new("(+ 4 1)").parse_expression()), 5);
+    assert_eq!(eval(&Tokenizer::new("(+ 321 48)").parse_expression()), 369);
+    
+    assert_eq!(eval(&Tokenizer::new("(* 4 6)").parse_expression()), 24);
+    assert_eq!(eval(&Tokenizer::new("(* 24 10)").parse_expression()), 240);
 
-//     assert_eq!(
-//         eval(&BinExpr {
-//             op: Operator::Multiply,
-//             left: 4,
-//             right: 1
-//         }),
-//         4
-//     );
-//     assert_eq!(
-//         eval(&BinExpr {
-//             op: Operator::Multiply,
-//             left: 3,
-//             right: 5
-//         }),
-//         15
-//     );
-//     assert_eq!(
-//         eval(&BinExpr {
-//             op: Operator::Multiply,
-//             left: 9,
-//             right: 0
-//         }),
-//         0
-//     );
-// }
+    assert_eq!(eval(&Tokenizer::new("(+ (* 23 11) (* 2 (+ 3 4)))").parse_expression()), 267);
+}
