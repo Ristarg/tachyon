1c8036155a02651ea59b7206abbe7305e19e98f0
Ristarg <a.filippov@protonmail.com>
2018-06-03 21:43:08 +0700

    Update grammar to more closely reflect current syntax, update builtin funcs to accept variable number of args

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,28 +1,27 @@
 use parser::*;
-use std::collections::HashMap;
+use std::{self, collections::HashMap};
 
 #[cfg(test)]
 mod tests;
 
-type FnBin = Fn(Vec<f64>) -> f64;
+type FnVec = Fn(Vec<f64>) -> f64;
 
 macro_rules! context {
     ($($key:expr => $value:expr),*) => ({
-        let mut tmp: HashMap<String, Box<FnBin>> = HashMap::new();
-        $(
-            tmp.insert($key.to_owned(), Box::new($value));
-        )*
+        let mut tmp: HashMap<String, Box<FnVec>> = HashMap::new();
+        $(tmp.insert($key.to_owned(), Box::new($value));)*
         tmp
     });
 }
 
 pub fn eval(source: &str) -> Option<f64> {
     let ctx = context!{
-        "+"    => |argv| argv[0] + argv[1],
-        "-"    => |argv| argv[0] - argv[1],
-        "*"    => |argv| argv[0] * argv[1],
-        "/"    => |argv| argv[0] / argv[1],
-        "test" => |_| 69.1337
+        "+"  => |argv| argv.iter().fold(0.0, |acc, x| acc + x),
+        "-"  => |argv| argv.iter().fold(0.0, |acc, x| acc - x),
+        "*"  => |argv| argv.iter().fold(1.0, |acc, x| acc * x),
+        "/"  => |argv| argv.iter().skip(1).fold(*argv.first().unwrap_or(&0.0), |acc, x| acc / x),
+        "pi" => |_| std::f64::consts::PI,
+        "e"  => |_| std::f64::consts::E
     };
 
     Parser::new(source)
@@ -30,11 +29,11 @@ pub fn eval(source: &str) -> Option<f64> {
         .and_then(|e| Some(eval_expr(&e, &ctx)))
 }
 
-fn eval_expr(expr: &Expr, ctx: &HashMap<String, Box<FnBin>>) -> f64 {
+fn eval_expr(expr: &Expr, ctx: &HashMap<String, Box<FnVec>>) -> f64 {
     match expr {
         Expr::Number(n) => *n,
         Expr::FnExprPtr(box expr) => match ctx.get(&expr.op.0) {
-            Some(fnbin) => fnbin(expr.args.iter().map(|a| eval_expr(a, &ctx)).collect()),
+            Some(func) => func(expr.args.iter().map(|a| eval_expr(a, &ctx)).collect()),
             None => panic!("no such function: \"{}\"", &expr.op.0),
         },
     }
